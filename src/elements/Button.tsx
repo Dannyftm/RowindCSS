7f7eac12d6b68b3253a69ca50d1bfcd467e1cd83
Danny <Dannyftm@users.noreply.github.com>
2024-10-12 19:42:38 -0700

    Update Button.tsx
    Added optional Disabled boolean to RowindProps

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -3,14 +3,15 @@ import RowindElement from "./RowindElement";
 
 interface RowindProps extends Roact.PropsWithChildren<{}> {
     className?: string
+    Disabled?: boolean,
     Event?: Roact.JsxInstanceEvents<TextButton> | undefined,
     Text?: string,
     ref?: Roact.Ref<TextButton>
 }
 
-export default ({className = "", Event, Text = "", ref}: RowindProps) => {
+export default ({className = "", Dsiabled, Event, Text = "", ref}: RowindProps) => {
     return (
-        <RowindElement ref={ref} Event={Event} Text={Text}
+        <RowindElement ref={ref} Disabled={Disabled} Event={Event}} Text={Text}
         tagName="button" className={className}/>
     )
 }
