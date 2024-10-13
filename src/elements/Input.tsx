9eed51e7999a5214053e3e9f19d3b53b2e19c006
Danny <Dannyftm@users.noreply.github.com>
2024-10-12 19:49:40 -0700

    Update Input.tsx
    Added optional Disabled boolean to RowindProps

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -4,13 +4,14 @@ import RowindElement from "./RowindElement";
 interface RowindProps extends Roact.PropsWithChildren<{}> {
     className?: string,
     Text?: string,
+    Disabled?: boolean,
     Event?: Roact.JsxInstanceEvents<TextBox> | undefined,
     ref?: Roact.Ref<TextBox>,
     placeholder?: string
 }
 
-export default ({className = "", Text = "", Event, ref, placeholder=""}: RowindProps) => {
+export default ({className = "", Text = "", Disabled, Event, ref, placeholder=""}: RowindProps) => {
     return (
-        <RowindElement placeholder={placeholder} ref={ref} Event={Event} tagName="input" Text={Text} className={className}/>
+        <RowindElement placeholder={placeholder} ref={ref} Disabled={Disabled} Event={Event} tagName="input" Text={Text} className={className}/>
     )
 }
